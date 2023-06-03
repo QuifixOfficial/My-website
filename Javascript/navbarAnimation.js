f44bb260373ce3f4b2645fe961e9963abea5a9ea
Quifix <42958246+andrygamer06@users.noreply.github.com>
2023-06-04 01:10:44 +0200

    This are the files. Steal them if you wish to do so

---
+++ Javascript/navbarAnimation.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.querySelector('.navbar');
+    navbar.style.opacity = '0.7';
+});
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+
+    if (window.scrollY > 50) {
+        navbar.style.opacity = '1.2';
+    } else {
+        navbar.style.opacity = '0.5';
+    }
+});
+function showAlert() {
+    alert("Do you really use facebook in 2023? Hehe");
+  }
+  
+  function rotateImage() {
+    var img = document.getElementById('img_5851');
+    img.classList.toggle('rotate');
+  }
+  
